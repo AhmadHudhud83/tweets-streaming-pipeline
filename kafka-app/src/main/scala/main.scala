d36c907faf4896001cc67bb2e14b769238ced3fb
AhmadHudhud83 <151885597+AhmadHudhud83@users.noreply.github.com>
2024-12-25 22:57:38 +0200

    Improved folder structuring for modularity

---
+++ kafka-app/src/main/scala/main.scala
@@ -0,0 +1,19 @@
+import com.johnsnowlabs.nlp.annotator._
+import com.johnsnowlabs.nlp._
+import org.apache.spark.ml._
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+import org.apache.log4j.BasicConfigurator
+import org.apache.log4j.varia.NullAppender
+object Main {
+  def main(args: Array[String]): Unit = {
+    val nullAppender = new NullAppender
+    BasicConfigurator.configure(nullAppender)
+
+
+    println("hello kafka")
+
+
+  }
+}
